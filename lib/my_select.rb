eb0ecd309cc3d8d7d6cf907a5b9b71f5bf7439ae
Matthew Shin <mshin1995@gmail.com>
2019-02-14 04:43:58 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,6 +3,9 @@ def my_select(collection)
   new_collection = []
     while i < collection.length 
       if yield collection[i] == true 
-      new_collection << collection[i]
+        new_collection << collection[i]
+      end
+      i += 1 
     end
+  new_collection
 end
